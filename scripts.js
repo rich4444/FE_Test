5ad481af02d0316166e1a0b2dade930b2cd24e43
Ricardo Nardochone <ricardog.n@hotmail.com>
2022-09-03 15:06:42 -0300

    All sliders done

--- scripts.js
+++ scripts.js
@@ -1,5 +1,21 @@
 const slidesTime = 5000;
 
+//Slide ---------------------------
+function slide(arr, fordward) {
+    arr.map((elem) => {
+        let currPos = elem.classList[1].charAt(3);
+        elem.classList.remove("pos" + currPos);
+        if (fordward) {
+            currPos++;
+            if (currPos > arr.length - 1) currPos = 0;
+        } else {
+            currPos--;
+            if (currPos < 0) currPos = arr.length - 1;
+        }
+        elem.classList.add("pos" + currPos);
+    });
+}
+
 //Info Cards Slider -----------------------------------------------
 const infoCards = [...document.getElementsByClassName("info-card")];
 const infoCards_bckBtn = document.getElementById("infoCards_bckBtn");
@@ -18,23 +34,62 @@ infoCards_nxtBtn.addEventListener("click", () => infoCardsSlide(false));
 var infoCardsInterval = setInterval(infoCardsSlide, slidesTime);
 
 function infoCardsSlide(fordward) {
+    //Clear the interval
     clearInterval(infoCardsInterval);
-
-    infoCards.map((card) => {
-        let newClass = card.classList[1].charAt(3);
-        card.classList.remove("pos" + newClass);
-        if (fordward) {
-            newClass++;
-            if (newClass > 4) newClass = 0;
-        } else {
-            newClass--;
-            if (newClass < 0) newClass = 4;
-        }
-        if (newClass > 4) newClass = 0;
-        card.classList.add("pos" + newClass);
-    });
-
+    //rotate the slider
+    slide(infoCards, fordward);
+    //Call again the interval
     infoCardsInterval = setInterval(() => infoCardsSlide(fordward), slidesTime);
 }
 
-//Remove posClasses if screen is bigger
+//Info Cards Slider -----------------------------------------------
+const perks = [...document.getElementsByClassName("perk")];
+const perks_bckBtn = document.getElementById("perks_bckBtn");
+const perks_nxtBtn = document.getElementById("perks_nxtBtn");
+
+//add classes from pos0 to pos4
+perks.reduce((acc, card) => {
+    card.classList.add("pos" + acc);
+    acc++;
+    return acc;
+}, 0);
+
+perks_bckBtn.addEventListener("click", () => perksSlide(true));
+perks_nxtBtn.addEventListener("click", () => perksSlide(false));
+
+var perksInterval = setInterval(perksSlide, slidesTime);
+
+function perksSlide(fordward) {
+    //Clear the interval
+    clearInterval(perksInterval);
+    //rotate the slider
+    slide(perks, fordward);
+    //Call again the interval
+    perksInterval = setInterval(() => perksSlide(fordward), slidesTime);
+}
+
+//Testimony Cards Slider -----------------------------------------------
+const testimony = [...document.getElementsByClassName("testimony")];
+const testimony_bckBtn = document.getElementById("testimonials_bckBtn");
+const testimony_nxtBtn = document.getElementById("testimonials_nxtBtn");
+
+//add classes from pos0 to pos4
+testimony.reduce((acc, card) => {
+    card.classList.add("pos" + acc);
+    acc++;
+    return acc;
+}, 0);
+
+testimony_bckBtn.addEventListener("click", () => testimonySlide(true));
+testimony_nxtBtn.addEventListener("click", () => testimonySlide(false));
+
+var testimonyInterval = setInterval(testimonySlide, slidesTime);
+
+function testimonySlide(fordward) {
+    //Clear the interval
+    clearInterval(testimonyInterval);
+    //rotate the slider
+    slide(testimony, fordward);
+    //Call again the interval
+    testimonyInterval = setInterval(() => testimonySlide(fordward), slidesTime);
+}
